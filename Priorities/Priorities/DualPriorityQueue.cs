4e0aaa424ec8e47fced322295e4b8ed07b0b3906
drewtorg <drew.torgeson@gmail.com>
2015-02-13 09:55:01 -0700

    Dual Priority Queue and Gitignore
    A fun and simple project, implementing a dual priority queue in C#.

---
+++ Priorities/Priorities/DualPriorityQueue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priorities
+{
+    public class DualPriorityQueue<TypeA,TypeB>
+    {
+        public int Count 
+        { 
+            get
+            {
+                return ListA.Count;
+            }
+        
+        }
+
+        public SortedList<TypeA, string> ListA { get; private set; }
+
+        public SortedList<TypeB, string> ListB { get; private set; }
+
+        public DualPriorityQueue()
+        {
+            ListA = new SortedList<TypeA, string>();
+            ListB = new SortedList<TypeB, string>();
+        }
+
+        public void Enqueue(string itemName, TypeA valueA, TypeB valueB)
+        {
+            ListA.Add(valueA, itemName);
+            ListB.Add(valueB, itemName);
+        }
+
+        public string DequeueA()
+        {
+            if (Count == 0)
+                throw new IndexOutOfRangeException();
+
+            string first = ListA.Values[0];
+            ListA.RemoveAt(0);
+            foreach(var keyVal in ListB)
+            {
+                if(keyVal.Value == first)
+                {
+                    ListB.Remove(keyVal.Key);
+                    break;
+                }
+            }
+            return first;
+        }
+
+        public string DequeueB()
+        {
+            string first = ListB.Values[0];
+            ListB.RemoveAt(0);
+            foreach (var keyVal in ListA)
+            {
+                if (keyVal.Value == first)
+                {
+                    ListA.Remove(keyVal.Key);
+                    break;
+                }
+            }
+            return first;
+        }
+
+        public void Clear()
+        {
+            ListA.Clear();
+            ListB.Clear();
+        }
+        public override string ToString()
+        {
+            string output = "";
+            foreach (var keyValA in ListA)
+                foreach (var keyValB in ListB)
+                    if (keyValA.Value == keyValB.Value)
+                        output += "Item: " + keyValB.Value + " " + keyValA.Key + " " + keyValB.Key + "\n";
+
+            return output;
+        }
+    }
+}
---
+++ Priorities/Priorities/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priorities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DualPriorityQueue<double, int> queue = new DualPriorityQueue<double, int>();
+            queue.Enqueue("Hyperion Hypodermic Needle", 1.99, 3);
+            queue.Enqueue("SuperSaver Syringe", .89, 7);
+            queue.Enqueue("InjectXpress Platinum Plated Needle", 2.49, 2);
+
+            //test that the Enqueues
+            Console.WriteLine(queue.ToString());
+
+            //test the Dequeues
+            Console.WriteLine(queue.DequeueA());
+            Console.WriteLine(queue.DequeueB());
+
+            //test the final result
+            Console.WriteLine(queue.ToString());
+        }
+    }
+}
